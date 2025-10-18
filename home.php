d881c058dae57e3b68c165b35768fd9b5177b321
Milottty <milotymeri1@gmail.com>
2025-10-18 12:44:30 +0200

    commit

--- home.php
+++ home.php
@@ -1,10 +1,28 @@
 <?php get_header(); ?>
 
 <h1>Hello, Digital School!</h1>
+<main class="blog-list">
+    <h1 class="blog-title">Lastest Blog Post</h1>
 
+    <?php if( have_posts()  ):?>
+        <?php while( have_posts() ): the_post(); ?>
+        <acticle <?php post_class('blog-item') ?>>
+            <h2 class="blog-item_title">
+                <a href="<?php the_permalink();?>"><?php the_title();?></a>
+            </h2>
+        
+            <p class="blog-item_excerpt">
+                <?php the_excerpt();?>
+            </p>
+        </acticle>
+        <?php endwhile; ?>
 
-    <?php if(have_posts()): while (have_posts()): the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
-    <?php endwhile; endif; ?>
+        <nav class="pagination">
+            <?php the_posts_pagination();?>
+        </nav>
+
+        <?php else:?>
+            <p>No post found</p>
+    <?php endif;?>
+</main>
 <?php get_footer(); ?>
